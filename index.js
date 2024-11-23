4d19b085f5a6b453ddd866f3b3ce67df7307a3c1
yt021 <yashart021gmail.com>
2024-11-23 16:38:08 +0330

    v1.0

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ $(".btn").click(function () {
   if (!started) {
     generateNumberSequence();
     for (var i = 0; i <= 15; i++) {
-      $("#i" + (i + 1)).attr("class", "item " + "p" + numberSequence[i]);
+      $(`#i${i + 1}`).attr("class", `item p${numberSequence[i]}`);
     }
     started = true;
     updateTimer();
     startTimer();
+    makeMovable();
+    timerFlashing("#timer");
   } else {
     startOver();
   }
@@ -23,18 +25,19 @@ $(".btn").click(function () {
 function startOver() {
   numberSequence = [];
   started = false;
-  clearInterval(interval);
-  minutes=0;
-  seconds=0;
+  stopTimer();
+  minutes = 0;
+  seconds = 0;
+  $(".yes").addClass("disabled");
+    $(".yes2").addClass("disabled");
+    $(".timer").removeClass("small-timer");
+    stopWinFlashing(".yes");
+  stopFlashing("#timer");
   $("#timer").text("00:00");
   $(".btn").text("Start");
   for (var i = 1; i <= 16; i++) {
-    $("#i" + i).attr("class", "item " + "p" + i);
+    $(`#i${i}`).attr("class", `item p${i}`);
   }
-}
-
-function move(input, i) {
-  $(input).attr("class", "item " + "p" + i);
 }
 
 function generateNumberSequence() {
@@ -44,7 +47,6 @@ function generateNumberSequence() {
       numberSequence.push(num);
     }
   }
-  console.log(numberSequence);
 }
 
 function updateTimer() {
@@ -52,8 +54,9 @@ function updateTimer() {
   var displaySeconds = seconds < 10 ? "0" + seconds : seconds;
   $("#timer").text(displayMinutes + ":" + displaySeconds);
 }
+
 function startTimer() {
-  interval = setInterval(function () {
+  timerInterval = setInterval(function () {
     seconds++;
     if (seconds === 60) {
       seconds = 0;
@@ -61,4 +64,100 @@ function startTimer() {
     }
     updateTimer();
   }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+}
+
+function calculateMoves() {
+  let availableMoves = [];
+  let position = parseInt(findClassName(16).slice(1));
+  let row = Math.floor((position - 1) / 4);
+  let col = (position - 1) % 4;
+  if (col > 0) {
+    availableMoves.push(position - 1);
+  }
+  if (col < 3) {
+    availableMoves.push(position + 1);
+  }
+  if (row > 0) {
+    availableMoves.push(position - 4);
+  }
+  if (row < 3) {
+    availableMoves.push(position + 4);
+  }
+  availableMoves = availableMoves.map((pos) => `p${pos}`);
+  return availableMoves;
+}
+
+function move(tile1, tile2Class) {
+  let tile1Class = findClassName(16);
+  $(tile1).attr("class", `item ${tile1Class}`);
+  $(`#i16`).attr("class", `item ${tile2Class}`);
+  makeMovable();
+  if (checkSequence()) {
+    stopTimer();
+    $(".yes").removeClass("disabled");
+    $(".yes2").removeClass("disabled");
+    $(".timer").addClass("small-timer");
+    winFlashing(".yes");
+  }
+}
+
+function makeMovable() {
+  let movables = calculateMoves();
+  $(".movable").off("click").removeClass("movable");
+  movables.forEach(function (moveClass) {
+    $(`.${moveClass}`)
+      .addClass("movable")
+      .click(function () {
+        move(this, moveClass);
+      });
+  });
+}
+
+function findClassName(input) {
+  let className = $(`#i${input}`)
+    .attr("class")
+    .split(" ")
+    .find((c) => c.startsWith("p"));
+  return className;
+}
+
+function checkSequence() {
+  for (let i = 1; i <= 16; i++) {
+    let classIndex = parseInt(findClassName(i).slice(1));
+    if (i !== classIndex) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function timerFlashing(selector) {
+  flashInterval = setInterval(function () {
+    $(selector).toggleClass("timer-flashing");
+  }, 1000);
+}
+
+function stopFlashing(selector) {
+  clearInterval(flashInterval);
+  $(selector).removeClass("timer-flashing");
+}
+
+function winFlashing(selector) {
+  let states = ['win-flashing1', 'win-flashing2', 'win-flashing3'];
+  let currentState = 0;
+  flashInterval = setInterval(function () {
+      $(selector).removeClass(states.join(' ')); 
+      $(selector).addClass(states[currentState]); 
+      currentState = (currentState + 1) % states.length; 
+  }, 333);
+}
+
+function stopWinFlashing(selector) {
+  clearInterval(flashInterval);
+  $(selector).removeClass("win-flashing1");
+  $(selector).removeClass("win-flashing2");
 }
